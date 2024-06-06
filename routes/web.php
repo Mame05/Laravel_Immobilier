bd997b89140df1f1b442ec96d9fa46a90877755c
artefarte001 <cheikhsane656@gmail.com>
2024-06-06 15:48:49 +0000

    finition avec le conflits

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,6 @@ Route::get('/bien', [BienController::class, 'ListeBien']);
 Route::get('bien/ajouter', [BienController::class, 'AjouterBien']);
 Route::post('/ajouter/bien-traitement', [BienController::class, 'AjouterBienTraitement']);
 
-<<<<<<< HEAD
 
 // route pour la modification de biens
 Route::post('/modifier/bien-traitement/', [BienController::class, 'ModifierBienTraitement']);
@@ -20,8 +19,6 @@ Route::get('/modifier-bien/{id}', [BienController::class, 'ModifierBien']);
 
 //suppression
 Route::get('/supprimer-bien/{id}', [BienController::class, 'SupprimerBien']);
-=======
 Route::get('/detail-bien/{id}', [BienController::class, 'DetailBien']);
 
 Route::post('/ajouter/commentaire-traitement', [CommentaireController::class, 'AjouterCommentaireTraitement']);
->>>>>>> 57c8ca9 (Création de la route et de la fonction pour l'ajout des commentaires et implémentation du formulaire d'ajout de commentaire dans la vue detail)
